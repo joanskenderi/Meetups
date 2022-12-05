d550f77560a439d1e69869330de2755a7c88a705
Joan Skënderi <jskenderi@outlook.com>
2022-12-05 01:53:13 +0100

    Final Commit🔥

---
+++ src/pages/NewMeetup.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const history = useHistory();
+
+  const addMeetupHandler = (meetupData) => {
+    // send the data through an HTTP request to Firebase
+    fetch(
+      "https://react-summary-udemy-fad44-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      // navigate to starting page after submitting the form
+      history.replace("/");
+    });
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
+
+export default NewMeetupPage;
